e112584485be34b65e4c7483a27cddfd94699241
MaximeGeorget <131368698+MaximeGeorget@users.noreply.github.com>
2025-05-15 20:05:43 +0200

    Added new bonuses

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,41 @@ import { Text, View, Button, GestureResponderEvent } from "react-native";
 import { bonus } from "@/lib/bonus";
 
 export default function Index() {
+  const [clicks, setClicks] = useState(0);
   const [score, setScore] = useState(0);
-  const [isBonusActive, setIsBonusActive] = useState(false);
+  const [scoredLastClick, setScoredLastClick] = useState(0);
+  const [bestScored, setBestScored] = useState(0);
+  const [maxBonusNumber, setMaxBonusNumber] = useState(5);
+  const [bonusNumber, setBonusNumber] = useState(0);
   const [activeBonusList, setActiveBonusList] = useState<typeof bonus>([]);
+
+  function calculateScore(actualScore: number) {
+    let newScore = 1;
+    for (let i = 0; i < activeBonusList.length; i++) {
+      newScore = activeBonusList[i].scoreModificator(newScore);
+    };
+    setScoredLastClick(newScore);
+    if (newScore > bestScored) {
+      setBestScored(newScore);
+    };
+    newScore = newScore + actualScore;
+    return newScore
+  }
+
   function whenButtonClicked(event: GestureResponderEvent) {
     const randomNumber = getRandomInt(5);
-    setScore(score + 1);
-    if (randomNumber === 0) {
-      // addBonus();
-      setIsBonusActive(true);
-      setActiveBonusList([...activeBonusList, bonus[0]])
+    setClicks((previousClicks) => {
+      return previousClicks + 1;
+    });
+    setScore(calculateScore(score));
+    if (randomNumber === 0 && bonusNumber < maxBonusNumber) {
+      // setActiveBonusList([...activeBonusList, bonus[getRandomInt(bonus.length)]]);
+      setActiveBonusList((previousActiveBonusList) => {
+        return [...previousActiveBonusList, bonus[getRandomInt(bonus.length)]]
+      });
+      setBonusNumber((previousBonusNumber) => {
+        return previousBonusNumber + 1;
+      });
     }
   }
   return (
@@ -23,9 +48,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Le score est : {score}</Text>
+      <Text>Clicks : {clicks}</Text>
+      <Text>Score : {score}</Text>
+      <Text>Scored last click : {scoredLastClick}</Text>
+      <Text>Best scored in one click : {bestScored}</Text>
       <Button title="Bouton" onPress={whenButtonClicked}></Button>
-      <Text>Le bonus est {isBonusActive ? "actif" : "inactif"}</Text>
+      <Text>Bonus : {bonusNumber}/{maxBonusNumber}</Text>
+      {/* <Text>Le bonus est {isBonusActive ? "actif" : "inactif"}</Text> */}
       {activeBonusList.map((activeBonus) => {
         return (
           <Text>{activeBonus.name}</Text>
--- lib/bonus.ts
+++ lib/bonus.ts
@@ -1,11 +1,44 @@
 export const bonus = [
     {
-        slug: "le-prems",
-        name: "Le prems",
+        slug: "plus-two",
+        name: "+ 2",
         rarity: "common",
         scoreModificator: (score: number) => {
             const modifiedScore = score + 2;
             return modifiedScore;
         },
     },
+    {
+        slug: "times-two",
+        name: "* 2",
+        rarity: "common",
+        scoreModificator: (score: number) => {
+            const modifiedScore = score * 2;
+            return modifiedScore;
+        },
+    },
+    {
+        slug: "plus-random",
+        name: "+ random",
+        rarity: "common",
+        scoreModificator: (score: number) => {
+            const modifiedScore = score + getRandomInt(10);
+            return modifiedScore;
+        },
+    },
+    {
+        slug: "times-random",
+        name: "* random",
+        rarity: "common",
+        scoreModificator: (score: number) => {
+            const modifiedScore = score * (1 + getRandomInt(3));
+            return modifiedScore;
+        },
+    },
 ]
+
+
+
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+}
